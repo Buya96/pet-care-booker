015552b9c6666053612c7a25378fe75811b5a8e1
Buya96 <ayub1231@hotmail.com>
2026-01-24 15:01:53 +0000

    feat(accounts): UserProfile model + migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class UserProfile(models.Model):
+    """Extra user info for pet bookings"""
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=True, max_length=200)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    try:
+        instance.userprofile.save()
+    except UserProfile.DoesNotExist:
+        UserProfile.objects.create(user=instance)
