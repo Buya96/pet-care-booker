4b296c05a194d2e6842bbedb96803da4ac56ef41
Buya96 <ayub1231@hotmail.com>
2026-01-25 16:10:35 +0000

    Add login page so users can sign in

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+# Commit 2: Auth URLs
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('login/', views.login, name='login'),  # Built-in Django login
+    path('logout/', views.LogoutView.as_view(next_page='login'), name='logout'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+# Signup view
+# User story: Visitors can create an account with additional profile information.
+
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import SignUpForm
 
 # Create your views here.
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = '/accounts/profile/'
+    template_name = 'accounts/signup.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # Auto-login after signup
+        user = form.save()
+        login(self.request, user)
+        return response
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    redirect_authenticated_user = True
+    success_url = '/'
+
+login = LoginView.as_view()
+
+
+
