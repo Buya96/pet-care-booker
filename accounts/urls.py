51ab684233f154d0d51e2ba30d7937d78e6270e5
Buya96 <ayub1231@hotmail.com>
2026-01-30 10:33:57 +0000

    fix urls.py for stripe integration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,17 +1,17 @@
-# Commit 2: Auth URLs
+# Commit 2: Auth URLs + Pay
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
-    path('login/', views.login, name='login'),  # Built-in Django login
-    path('logout/', views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('login/', views.login, name='login'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('pay/', views.create_checkout_session, name='pay'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('services/', views.services, name='services'),
     path('book/', views.BookingView.as_view(), name='book'),
     path('booking/<int:pk>/update/', views.BookingUpdateView.as_view(), name='booking_update'),
     path('booking/<int:pk>/delete/', views.BookingDeleteView.as_view(), name='booking_delete'),
     path('bookings/', views.UserBookingsView.as_view(), name='bookings'),
-    
 ]
